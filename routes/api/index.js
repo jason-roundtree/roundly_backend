c88f332d9a69f9f55699cc8520b326e6adf40c6e
Jason Roundtree <jasontree123@gmail.com>
2023-12-08 00:00:30 -0800

    PointSetting POST working

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,6 +6,7 @@ const playerRoutes = require("./playerRoutes");
 // const leaguePlayerRoutes = require("./leaguePlayerRoutes");
 const roundRoutes = require("./roundRoutes");
 const playerRoundRoutes = require("./playerRoundRoutes");
+const pointSettingsRoutes = require("./pointSettingsRoutes");
 
 router.use("/leagues", leagueRoutes);
 router.use("/users", userRoutes);
@@ -14,5 +15,6 @@ router.use("/players", playerRoutes);
 // router.use("/league-players", leaguePlayerRoutes);
 router.use("/rounds", roundRoutes);
 router.use("/player-rounds", playerRoundRoutes);
+router.use("/point-settings", pointSettingsRoutes);
 
 module.exports = router;
--- routes/api/pointSettingsRoutes.js
+++ routes/api/pointSettingsRoutes.js
@@ -1,4 +1,69 @@
 const router = require("express").Router();
-const {} = require("../../models");
+const { PointSetting } = require("../../models");
+
+router.get("/:id", async (req, res) => {
+  console.log("get PointSetting by id route");
+  try {
+    const data = await PointSetting.findByPk(req.params.id);
+    console.log("data: ", data);
+    if (!data) {
+      res.status(404).json({ message: "No matching point setting" });
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("get PointSetting by id err: ", err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  console.log("create PointSetting req.body: ", req.body);
+  try {
+    const data = await PointSetting.create(req.body);
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("create PointSetting err: ", err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = await PointSetting.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("delete PointSetting err: ", err);
+    res.status(500).json(err);
+  }
+});
+
+// router.put("/", async (req, res) => {
+//   try {
+//     const [affectedRows] = await PointSetting.update(req.body);
+//     res.status(204).end();
+//   } catch (err) {
+//     console.log("update PointSettings err: ", err);
+//     res.status(500).json(err);
+//   }
+// });
+
+router.put("/:id", async (req, res) => {
+  try {
+    const [affectedRows] = await PointSetting.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(204).end();
+  } catch (err) {
+    console.log("update PointSetting err: ", err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
