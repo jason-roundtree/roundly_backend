5f2d3ba4d385fe42dbc95b2f976ee98aaef84c4f
Jason Roundtree <jasontree123@gmail.com>
2024-06-21 23:13:30 -0700

    setup basic GET for player-points-earned-in-round route

--- routes/api/playerPointEarnedRoutes.js
+++ routes/api/playerPointEarnedRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { PlayerPointEarned } = require("../../models");
+// const sequelize = require("../../db_connection");
+const { PlayerPointEarned, PointSetting } = require("../../models");
 
 module.exports = router;
 
@@ -13,3 +14,47 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// GET individual points earned by player in round
+router.get("/player/:playerId/round/:roundId", async (req, res) => {
+  console.log("get PlayerPointEarned by round");
+  try {
+    const data = await PlayerPointEarned.findAll({
+      //   attributes: {
+      //     include: [[sequelize.fn("SUM", sequelize.col("value")), "sum_values"]],
+      //   },
+      where: {
+        playerId: req.params.playerId,
+        roundId: req.params.roundId,
+      },
+      include: [
+        {
+          model: PointSetting,
+          //   attributes: [
+          //     "id",
+          //     "name",
+          //     "value",
+          //     "scope",
+          //     "isLeagueSetting",
+          //     "maxFrequencyPerScope",
+          //   ],
+        },
+      ],
+    });
+    console.log("PlayerPointEarned by round data: ", data);
+    // TODO: should this only check for !data.length? Are there other routes made where I should be checking for empty array instead?
+    if (!data || !data.length) {
+      res
+        .status(404)
+        .json({ message: "No matching player round points found" });
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("get PlayerPointEarned by round err: ", err);
+    res.status(500).json(err);
+  }
+});
+
+// TODO:
+// GET sum of points earned by player in round
