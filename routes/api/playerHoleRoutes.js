acf00c59b55151b3f6af5fde4baa8fce858e7b3f
Jason Roundtree <jasontree123@gmail.com>
2024-06-23 16:40:08 -0700

    Add PUT and DELETE player hole routes; separate playerPointsEarnedRoutes

--- routes/api/playerHoleRoutes.js
+++ routes/api/playerHoleRoutes.js
@@ -4,12 +4,49 @@ const { PlayerHole } = require("../../models");
 module.exports = router;
 
 router.post("/", async (req, res) => {
-  console.log("create PlayerHole req.body: ", req.body);
+  console.log("find or create PlayerHole req.body: ", req.body);
+  const { playerId, roundId, hole } = req.body;
+  const whereValues = {
+    playerId,
+    roundId,
+    hole,
+  };
   try {
-    const data = await PlayerHole.create(req.body);
+    const data = await PlayerHole.findOrCreate({
+      where: whereValues,
+      default: req.body,
+    });
     res.status(200).json(data);
   } catch (err) {
     console.log("create PlayerHole err: ", err);
     res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  console.log("update PlayerHole req.body: ", req.body);
+  try {
+    const [affectedRows] = await PlayerHole.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(204).end();
+  } catch (err) {
+    console.log("update PlayerHole err: ", err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  console.log("delete PlayerHole req.body: ", req.body);
+  try {
+    const data = await PlayerHole.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("delete PlayerHole err: ", err);
+    res.status(500).json(err);
   }
 });
---
+++ routes/api/playerPointsEarnedRoutes.js
@@ -0,0 +1,96 @@
+const router = require("express").Router();
+const sequelize = require("../../db_connection");
+const { QueryTypes } = require("sequelize");
+const {
+  PlayerPointEarned,
+  PointSetting,
+  Player,
+  Round,
+} = require("../../models");
+
+module.exports = router;
+
+// GET individual points earned by player in round
+router.get("/player/:playerId/round/:roundId", async (req, res) => {
+  console.log("get PlayerPointEarned by round");
+  try {
+    const data = await PlayerPointEarned.findAll({
+      where: {
+        playerId: req.params.playerId,
+        roundId: req.params.roundId,
+      },
+      include: [
+        {
+          model: PointSetting,
+          //   attributes: [
+          //     "id",
+          //     "name",
+          //     "value",
+          //     "scope",
+          //     "isLeagueSetting",
+          //     "maxFrequencyPerScope",
+          //   ],
+        },
+        { model: Player, attributes: ["name"] },
+      ],
+    });
+    console.log("PlayerPointEarned by round data: ", data);
+    // TODO: should this only check for !data.length? Are there other routes made where I should be checking for empty array?
+    if (!data || !data.length) {
+      res
+        .status(404)
+        .json({ message: "No matching player round points found" });
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("get PlayerPointEarned by round err: ", err);
+    res.status(500).json(err);
+  }
+});
+
+// GET total points earned by player in round
+router.get(
+  "/player/:playerId/round/:roundId/total-points",
+  async (req, res) => {
+    console.log("get sum of PlayerPointEarned by round");
+    const q = `
+        select 
+            player.name as player_name,
+            player.id as player_id,
+            sum(ps.value * ppe.frequency) as point_sum
+        from player_point_earned ppe
+        join point_setting ps 
+            on ppe.point_setting_id = ps.id
+        join player
+            on ppe.player_id = player.id
+        join round
+            on ppe.round_id = round.id
+        where 
+            round.id = ?
+            and
+            player.id = ?
+        group by  
+            player.id,
+            player.name;
+    `;
+    try {
+      const data = await sequelize.query(q, {
+        replacements: [req.params.roundId, req.params.playerId],
+        type: QueryTypes.SELECT,
+      });
+      console.log("sum of PlayerPointEarned by round data: ", data);
+
+      if (!data || !data.length) {
+        res
+          .status(404)
+          .json({ message: "No matching player round points found" });
+        return;
+      }
+      res.status(200).json(data);
+    } catch (err) {
+      console.log("get PlayerPointEarned by round err: ", err);
+      res.status(500).json(err);
+    }
+  }
+);
