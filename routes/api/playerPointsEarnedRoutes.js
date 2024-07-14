e16d0d73f215143a470fafa6414ab991a27e76ea
Jason Roundtree <jasontree123@gmail.com>
2024-07-13 23:32:17 -0700

    Add routes for player round points by point type and hole

--- routes/api/playerPointsEarnedRoutes.js
+++ routes/api/playerPointsEarnedRoutes.js
@@ -10,9 +10,9 @@ const {
 
 module.exports = router;
 
-// GET individual points earned by player in round
+// GET player round points earned
 router.get("/player/:playerId/round/:roundId", async (req, res) => {
-  console.log("get PlayerPointEarned by round");
+  console.log("--> GET player round points earned");
   try {
     const data = await PlayerPointEarned.findAll({
       where: {
@@ -36,11 +36,13 @@ router.get("/player/:playerId/round/:roundId", async (req, res) => {
       ],
     });
     console.log("PlayerPointEarned by round data: ", data);
-    // TODO: should this only check for !data.length? Are there other routes made where I should be checking for empty array?
-    if (!data || !data.length) {
-      res
-        .status(404)
-        .json({ message: "No matching player round points found" });
+    // TODO: is 204 ok to use and is 404 needed?
+    if (!data.length) {
+      res.status(204).json({ message: "No player round points found" });
+      return;
+    }
+    if (!data) {
+      res.status(404).json({ message: "uhhh, something else happened" });
       return;
     }
     res.status(200).json(data);
@@ -50,11 +52,100 @@ router.get("/player/:playerId/round/:roundId", async (req, res) => {
   }
 });
 
+// GET player round points earned by point type
+router.get(
+  "/player/:playerId/round/:roundId/point/:pointSettingId",
+  async (req, res) => {
+    console.log("--> GET player round points earned by point type");
+    try {
+      const data = await PlayerPointEarned.findAll({
+        where: {
+          playerId: req.params.playerId,
+          roundId: req.params.roundId,
+          pointSettingId: req.params.pointSettingId,
+        },
+        include: [
+          {
+            model: PointSetting,
+            //   attributes: [
+            //     "id",
+            //     "name",
+            //     "value",
+            //     "scope",
+            //     "isLeagueSetting",
+            //     "maxFrequencyPerScope",
+            //   ],
+          },
+          { model: Player, attributes: ["name"] },
+          { model: PlayerHole },
+        ],
+      });
+      console.log("PlayerPointEarned by point type data: ", data);
+      // TODO: should this only check for !data.length? Are there other routes made where I should be checking for empty array?
+      if (!data || !data.length) {
+        res
+          .status(404)
+          .json({ message: "No matching player round points found" });
+        return;
+      }
+      res.status(200).json(data);
+    } catch (err) {
+      console.log("PlayerPointEarned by point type err: ", err);
+      res.status(500).json(err);
+    }
+  }
+);
+
+// GET player round points earned by hole
+router.get(
+  "/player/:playerId/round/:roundId/hole/:holeNumber",
+  async (req, res) => {
+    console.log("--> GET player round points earned by hole");
+    try {
+      const data = await PlayerPointEarned.findAll({
+        where: {
+          playerId: req.params.playerId,
+          roundId: req.params.roundId,
+        },
+        include: [
+          {
+            model: PointSetting,
+            //   attributes: [
+            //     "id",
+            //     "name",
+            //     "value",
+            //     "scope",
+            //     "isLeagueSetting",
+            //     "maxFrequencyPerScope",
+            //   ],
+          },
+          { model: Player, attributes: ["name"] },
+          { model: PlayerHole, where: { hole: req.params.holeNumber } },
+        ],
+      });
+      console.log("PlayerPointEarned by hole data: ", data);
+      // TODO: is 204 ok to use and is 404 needed?
+      if (!data.length) {
+        res.status(204).json({ message: "No player round points found" });
+        return;
+      }
+      if (!data) {
+        res.status(404).json({ message: "uhhh, something else happened" });
+        return;
+      }
+      res.status(200).json(data);
+    } catch (err) {
+      console.log("get PlayerPointEarned by round err: ", err);
+      res.status(500).json(err);
+    }
+  }
+);
+
 // GET total points earned by player in round
 router.get(
   "/player/:playerId/round/:roundId/total-points",
   async (req, res) => {
-    console.log("get sum of PlayerPointEarned by round");
+    console.log("--> GET total points earned by player in round");
     const q = `
         select 
             player.name as player_name,
